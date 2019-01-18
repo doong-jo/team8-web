6a50d5e959e1b33e569073b0b5bbad7d914c8203
hyungjin Park <gudwlssla45@naver.com>
2019-01-18 13:29:58 +0900

    resolve conflict

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,1 @@
-<<<<<<< Updated upstream
-export { default as UserView } from './userView/index';
-export { default as DeviceView} from './deviceView/index';
-=======
 export { default as Main } from './main/index';
->>>>>>> Stashed changes
