82d1a30e721b4e7bf3157277c0df6e3b39631b1e
hyungjin Park <gudwlssla45@naver.com>
2019-01-18 13:22:45 +0900

    reslove conflicts

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,7 @@ const router = express.Router();
 
 // const template = require('./template');
 	  
-<<<<<<< Updated upstream
-const main = require('./router/main'),
-    user = require('./router/user'),
-    userview = require('./router/userview'),
-    tracking = require('./router/tracking'),
-    accident = require("./router/accident"),
-    led = require("./router/led"),
-    category = require("./router/category"),
-    device = require("./router/deviceview");
-
-router.get('/', function(req, res) { res.redirect('/main'); });
-router.use('/main', main);
-router.use('/tracking', tracking);
-router.use('/user', user);
-router.use('/accident', accident);
-router.use('/led', led);
-router.use('/category', category);
-
-//page
-router.use('/userview', userview);
-router.use('/deviceview', device);
-
-router.get('*', function(req, res) {
-    res.json('접근할 수 없는 페이지입니다.');
-});
-
-=======
 const main = require('./router/main');
 router.use('/', main);
->>>>>>> Stashed changes
 
 module.exports = router;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
-import * as d3 from 'd3';
-import DashboardCard from './components/DashboardCard';
-import ToolBar from '../../components/common/ToolBar';
-import { timeFormatter } from '../../page';
 import styles from './style.scss';
 
 class Main extends React.Component {
@@ -17,10 +12,6 @@ class Main extends React.Component {
     }
     render() {
         return (
-<<<<<<< Updated upstream
-            <div>
-                
-=======
             <div className={styles.Main__content}>
                 <img className={styles.Main__content__image} src="../../../images/teameight_logo.png"/>
                 <p>
@@ -32,7 +23,6 @@ class Main extends React.Component {
                     현재 홈페이지 수리 중입니다. 잠시만 기다려 주시기 바랍니다.
     			</p>
                 <a href="mailto:EIGHT@8riders.com">EIGHT@8riders.com</a>
->>>>>>> Stashed changes
             </div>
         );
     }
