c6d8d506fb0b221414343d96540181b7f4c87f5c
hyungjin Park <gudwlssla45@naver.com>
2019-01-18 13:24:31 +0900

    resolve conflict

--- routes/template.js
+++ routes/template.js
@@ -3,11 +3,7 @@ var template = function (initStateObj) {
     <!DOCTYPE html>
     <html>
         <head>
-<<<<<<< Updated upstream
             <link rel="stylesheet" href="helper-bootstrap.css">
-=======
-            <link rel="stylesheet" href="http://175.123.140.145/helper-bootstrap.css">
->>>>>>> Stashed changes
             <script>window.APP_INITIAL_STATE = `+JSON.stringify(initStateObj)+`;</script>
             <title>TEAM EIGHT</title>
         </head>
@@ -16,11 +12,7 @@ var template = function (initStateObj) {
             <div id="root"></div>
         </body>
 
-<<<<<<< Updated upstream
         <script src="bundle.js"></script>
-=======
-        <script src="http://175.123.140.145/bundle.js"></script>
->>>>>>> Stashed changes
     </html>
   `;
 };
